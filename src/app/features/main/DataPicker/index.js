179a40369078c8ef46e2a52de97564a5096bcc47
Vladimir Baraniuk <norb4kivlad@gmail.com>
2024-11-30 09:18:20 +0200

    fix: data picker hook

---
+++ src/app/features/main/DataPicker/index.js
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AIR_OPTION, FAVORITE_OPTION, POPULAR_OPTION } from "../../../components/dropdown/utils/index.js";
+import { getFavoriteRequest } from "../../../redux/slices/favorite/index.js";
+import { getAirRequest, getPopularRequest } from "../../../redux/slices/movie/index.js";
+
+const useDataPicker = (category) => {
+    const dispatch = useDispatch();
+    const { popularData, airData } = useSelector((state) => state.movieReducer);
+    const { data } = useSelector((state) => state.favoriteReducer);
+
+    useEffect(() => {
+        switch (category) {
+            case FAVORITE_OPTION:
+                dispatch(getFavoriteRequest({ page: 1, language: "en-US" }));
+                break;
+            case POPULAR_OPTION:
+                dispatch(getPopularRequest({ page: 1, language: "en-US" }));
+                break;
+            case AIR_OPTION:
+                dispatch(getAirRequest({ page: 1, language: "en-US" }));
+                break;
+            default:
+                break;
+        }
+    }, [category, dispatch]); // Вызывается только при изменении category
+
+    switch (category) {
+        case FAVORITE_OPTION:
+            return data;
+        case POPULAR_OPTION:
+            return popularData;
+        case AIR_OPTION:
+            return airData;
+        default:
+            return {};
+    }
+};
+
+export default useDataPicker;
--- src/app/pages/main/index.jsx
+++ src/app/pages/main/index.jsx
@@ -1,35 +1,22 @@
-import {useEffect, useState} from 'react';
+import {useState} from 'react';
 import Dropdown from "../../components/dropdown/index.jsx";
 import {} from './main.css'
 import Movie from "../../components/movie/index.jsx";
-import {useDispatch, useSelector} from "react-redux";
-import {getAirRequest} from "../../redux/slices/movie/index.js";
-import {movieCategory, POPULAR_OPTION} from "../../components/dropdown/utils/index.js";
-import {getFavoriteRequest} from "../../redux/slices/favorite/index.js";
+import {FAVORITE_OPTION, movieCategory} from "../../components/dropdown/utils/index.js";
+import useDataPicker from "../../features/main/DataPicker/index.js";
 
 
 const MainPage = () => {
-    const dispatch = useDispatch();
-    const { popularData, airData} = useSelector(
-        (state) => state.movieReducer);
-    const {data} = useSelector(state => state.favoriteReducer)
-    const [category, setCategory] = useState(POPULAR_OPTION)
+    const [category, setCategory] = useState(FAVORITE_OPTION)
+    let displayedData = useDataPicker(category);
 
-    useEffect(() => {
-        dispatch(getFavoriteRequest({ page: 1, language: "en-US" }));
-
-    }, [dispatch]);
-
-    useEffect(() => {
-        console.log(airData)
-    },[airData])
 
     return (
         <div className={'main-page_container'}>
             <Dropdown optionList={movieCategory} onChange={setCategory}/>
             <div className={'movie-list_container'}>
-                {Array.isArray(data.results) && data.results.length > 0 ? (
-                    data.results?.map((data) => (
+                {Array.isArray(displayedData.results) && displayedData.results.length > 0 ? (
+                    displayedData.results?.map((data) => (
                         <Movie key={data.id} data={data}/>
                     ))
                 ) : (

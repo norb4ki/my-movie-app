ee3aa74ea7625253fc396618c735739eb2b56c7b
Vladimir Baraniuk <norb4kivlad@gmail.com>
2024-11-30 10:00:31 +0200

    feat: add custom dropdown

--- src/app/components/dropdown/index.jsx
+++ src/app/components/dropdown/index.jsx
@@ -1,16 +1,42 @@
-import {} from './dropdown.css'
+import React, { useState } from "react";
+import "./dropdown.css";
 
-const Dropdown = ({optionList, onChange}) => {
+const CustomDropdown = ({ optionList, onChange }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(optionList[0]);
+
+    const toggleDropdown = () => setIsOpen(!isOpen);
+
+    const handleSelect = (option) => {
+        setSelectedOption(option);
+        onChange(option.value);
+        setIsOpen(false);
+    };
 
     return (
-        <select onChange={(e) => onChange(e.target.value)}>
-            {optionList.map((option) => (
-                <option key={option.value} value={option.value}>
-                    {option.label}
-                </option>
-            ))}
-        </select>
+        <div className="dropdown">
+            <div
+                className="dropdown_header"
+                onClick={toggleDropdown}
+            >
+                {selectedOption ? selectedOption.label : "Select an option"}
+                <span className={`dropdown_arrow ${isOpen ? "open" : ""}`}>▼</span>
+            </div>
+            {isOpen && (
+                <ul className="dropdown_list">
+                    {optionList.map((option) => (
+                        <li
+                            key={option.value}
+                            className="dropdown_item"
+                            onClick={() => handleSelect(option)}
+                        >
+                            {option.label}
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     );
 };
 
-export default Dropdown;
+export default CustomDropdown;
